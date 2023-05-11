5b591992f452af7713b634c539fca0218df7ca94
Yasha-Thakur <76092518+Yasha-Thakur@users.noreply.github.com>
2023-05-11 14:48:19 +0530

    Update otp.js (debug)

--- routes/otp.js
+++ routes/otp.js
@@ -3,9 +3,8 @@ const axios = require('axios');
 const express = require('express');
 const cors = require('cors')
 const router = express.Router()
-router.options('/sendotp', cors())
 
-router.post('/sendotp', cors(), async (req, res) => {
+router.post('/sendotp', async (req, res) => {
     const { apikey, userEmail, redirectUrl } = req.body
     console.log(req.get('host'));
     console.log(req);
